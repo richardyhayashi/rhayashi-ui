64ff2e6c1e9588a2aa45e7246585b28a1ec4bc54
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-03-27 18:06:48 +0900

    First component Button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+      }
+    },
   },
   plugins: [],
 }
