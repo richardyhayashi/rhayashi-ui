574652829191a0f170c368f69448c46734ca2be7
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-03-27 20:43:27 +0900

    Add Layout component and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "RhayashiUI",
+      fileName: "rhayashi-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        }
+      }
+    }
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
 })
